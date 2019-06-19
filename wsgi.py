a4e12f7671b1759ecdc39db3f5745e731aa29e7d
Stéphane MENANTEAU <stephane.a.menanteau@socgen.com>
2019-06-19 11:43:24 +0200

    Implementation of READ function

--- wsgi.py
+++ wsgi.py
@@ -2,19 +2,24 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+
+PRODUCTS = [
+    {'id': 1, 'name': 'Skello'},
+    {'id': 2, 'name': 'Socialive.tv'},
+    {'id': 3, 'name': 'Twitter'}
+]
+
 @app.route('/')
 def hello():
     return "Hello World!"
 
-@app.route('/product')
-def product():
-    return "No product at least..."
-
 @app.route('/api/v1/products')
 def products():
-    PRODUCTS = [
-        {'id': 1, 'name': 'Skello'},
-        {'id': 2, 'name': 'Socialive.tv'},
-        {'id': 3, 'name': 'Twitter'}
-    ]
     return jsonify(PRODUCTS) 
+
+@app.route('/api/v1/products/<int:id>')
+def read(id):
+    try:
+        return jsonify(PRODUCTS[id-1])
+    except:
+        return 'Product key ' + str(id) + ' does not exist!'
