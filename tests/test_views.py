07cf19c6cb1432a40a94fc09c30da053572f6d70
Stéphane MENANTEAU <stephane.a.menanteau@socgen.com>
2019-06-19 14:33:22 +0200

    Implementation of DELETE function

--- tests/test_views.py
+++ tests/test_views.py
@@ -12,3 +12,13 @@ class TestViews(TestCase):
         products = response.json
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_products_unknown(self):
+        response = self.client.get("/api/v1/products/4")
+        products = response.json
+        print(products, '404')
+
+    def test_products_delete(self):
+        response = self.client.delete("/api/v1/products/2")
+        answer = response.json
+        print(answer)
--- wsgi.py
+++ wsgi.py
@@ -1,25 +1,43 @@
 # wsgi.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, render_template, request, make_response
 app = Flask(__name__)
 
 
 PRODUCTS = [
-    {'id': 1, 'name': 'Skello'},
     {'id': 2, 'name': 'Socialive.tv'},
-    {'id': 3, 'name': 'Twitter'}
+    {'id': 3, 'name': 'Twitter'},
+    {'id': 1, 'name': 'Skello'}
 ]
+
+PRODUCTS_MAP = {
+    item['id']: item
+    for item in PRODUCTS
+}
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
 @app.route('/api/v1/products')
-def products():
+def get_products():
     return jsonify(PRODUCTS) 
 
 @app.route('/api/v1/products/<int:id>')
-def read(id):
-    try:
-        return jsonify(PRODUCTS[id-1])
-    except:
-        return 'Product key ' + str(id) + ' does not exist!'
+def get_product(id):
+    result = PRODUCTS_MAP.get(id)
+    if result:
+        return jsonify(result)
+    abort(404)
+
+@app.route('/api/v1/products/<int:id>', methods=['DELETE'])
+def del_product(id):
+    result = PRODUCTS_MAP.get(id)
+    if result:
+        del PRODUCTS_MAP[id]
+        return jsonify(PRODUCTS_MAP)
+        make_response('Item deleted', 200)
+    abort(404)
