ab43dceaad7218517c96601051355d1702789677
Stéphane MENANTEAU <stephane.a.menanteau@socgen.com>
2019-06-19 11:09:10 +0200

    First version of our REST API

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,3 +9,12 @@ def hello():
 @app.route('/product')
 def product():
     return "No product at least..."
+
+@app.route('/api/v1/products')
+def products():
+    PRODUCTS = [
+        {'id': 1, 'name': 'Skello'},
+        {'id': 2, 'name': 'Socialive.tv'},
+        {'id': 3, 'name': 'Twitter'}
+    ]
+    return jsonify(PRODUCTS) 
